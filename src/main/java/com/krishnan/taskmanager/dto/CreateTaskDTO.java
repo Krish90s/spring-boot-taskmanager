de61d9c99e930cc73173bfa131f7345cf5d3dd9e
Krish90s <krishnanvenkat4@gmail.com>
2023-03-23 21:09:10 +0530

    Add existing file

---
+++ src/main/java/com/krishnan/taskmanager/dto/CreateTaskDTO.java
@@ -0,0 +1,14 @@
+package com.krishnan.taskmanager.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class CreateTaskDTO {
+    String title;
+    String description;
+    String deadline;
+}
